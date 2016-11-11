39f34adbfc01b7d05e3274184f4f376a883240f2
kgkaudi <a16kongk@hs.local>
2016-11-11 11:41:12 +0100

    Alarms
    Alarm system has been created

---
+++ Assets/Scripts/Alarm.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Alarm : MonoBehaviour {
+
+	GameObject [] alarmBoxes;
+
+	private int i;
+
+	void OnTriggerEnter(Collider other){
+		Debug.Log ("Triggered!!!!!");
+		alarmBoxes = GameObject.FindGameObjectsWithTag ("Alarm");
+
+		for ( i = 0; i < alarmBoxes.Length; i++ ){
+			alarmBoxes[i].GetComponent<Collider>().enabled = false;
+		}
+	}
+}
