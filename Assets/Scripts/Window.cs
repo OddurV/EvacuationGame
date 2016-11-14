a01dc8ea1996d88f70d3f0d06a3ccae698acffb6
kgkaudi <a16kongk@hs.local>
2016-11-14 13:21:05 +0100

    Elevator Prefab & Windows
    Added Elevator prefab and functioning closing windows

---
+++ Assets/Scripts/Window.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Window : MonoBehaviour {
+
+	public float AngleY;
+
+	private float targetValue = 0f;
+	private float currentValue = 180f;
+	private float easing = 0.05f;
+
+	void Update () {
+		currentValue = currentValue + (targetValue - currentValue) * easing;
+		transform.rotation = Quaternion.identity;
+		transform.Rotate(0, 0, currentValue);
+	}
+
+	void OnTriggerEnter(Collider other){
+		Debug.Log ("Closed!!!!!");
+		GetComponent<Collider>().enabled = false;
+		targetValue = AngleY;
+		currentValue = 0;
+	}
+}
