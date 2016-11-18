2b64042fd513cd570767246b1279c44adda562fa
OddurV <a16oddvi@hs.local>
2016-11-18 11:59:34 +0100

    Pause menu and graphics for main menu
    Manual merge from Anthony's branch

---
+++ Assets/Scripts/PauseController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
+
+public class PauseController : MonoBehaviour {
+    public Transform pause;
+	// Update is called once per frame
+	void Update () {
+        
+       if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            Pause();
+        }
+	}
+
+    public void Pause()
+    {
+            if (pause.gameObject.activeInHierarchy == false)
+            {
+                pause.gameObject.SetActive(true);
+                Time.timeScale = 0;
+            }
+            else
+            {
+                pause.gameObject.SetActive(false);
+                Time.timeScale = 1;
+            }
+
+    }
+}
