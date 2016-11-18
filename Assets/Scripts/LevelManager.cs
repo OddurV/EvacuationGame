a32137d716da4baebc200ff71d24b3adaf3cfdec
OddurV <a16oddvi@hs.local>
2016-11-18 12:11:20 +0100

    merge test

--- Assets/Scripts/LevelManager.cs
+++ Assets/Scripts/LevelManager.cs
@@ -1,8 +1,11 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
 
 public class LevelManager : MonoBehaviour {
+
+    public Transform mainMenu, optionsMenu;
 
 	public void LoadScene(string name){
         //Application.LoadLevel(name);
@@ -11,5 +14,40 @@ public class LevelManager : MonoBehaviour {
     public void QuitGame()
     {
         Application.Quit();
+    }
+
+    public void OpenOptionsMenu (bool clicked)
+    {
+        if (clicked == true)
+        {
+            optionsMenu.gameObject.SetActive(clicked);
+            mainMenu.gameObject.SetActive(!clicked);
+        }
+        else
+        {
+            optionsMenu.gameObject.SetActive(clicked);
+            mainMenu.gameObject.SetActive(true);
+        }
+    }
+
+    public void OpenItemOptionsMenu()
+    {
+        string buttonName = EventSystem.current.currentSelectedGameObject.name;
+        switch (buttonName)
+        {
+            case "ButtonControls":
+                
+                break;
+            case "ButtonPlayerSettings":
+                
+                break;
+            case "ButtonMusic":
+
+                break;
+            case "ButtonRules":
+
+                break;
+
+        }
     }
 }
