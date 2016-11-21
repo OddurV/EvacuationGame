c250cf01f2a44a1826f0c09a85909289e0977bf6
Luis Hernández <alecshr@gmail.com>
2016-11-21 12:28:31 +0100

    Small fire now spawn in couch
    I have added a FireManager as a child of GameManager, there is a script
    for spawning small fires every 10 seconds, but the first one starts
    after the first 40 seconds of game start.

---
+++ Assets/FireStart.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FireStart : MonoBehaviour {
+
+	public float timeRemaining = 40f;
+	public GameObject smallFire;
+	public float spawnSmallFireTime = 10;
+	public Transform [] spawnFirePoints;
+
+	// Use this for initialization
+	void Start () {
+		
+		InvokeRepeating ("SpawnSmallFire", timeRemaining, spawnSmallFireTime);
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	void StartFire() {
+		Debug.Log ("StartFire!", gameObject);
+		SpawnSmallFire ();
+	}
+
+	void SpawnSmallFire () {
+		int spawnPointIndex = Random.Range (0, spawnFirePoints.Length);
+		Instantiate (smallFire, spawnFirePoints[spawnPointIndex].position, spawnFirePoints[spawnPointIndex].rotation);
+	}
+}
