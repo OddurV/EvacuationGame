7052a5f8748842fd4507c4dbcd30092d66cc2f15
kgkaudi <a16kongk@hs.local>
2016-11-22 11:23:30 +0100

    Health And Death
    Player losses health on smoke and on death the level restarts

---
+++ Assets/Scripts/PlayerHealth.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerHealth : MonoBehaviour {
+
+    public Text healthText;
+    public int startingHealth;
+    public int currentHealth;
+    public bool harmed = false;
+    
+    // Use this for initialization
+    void Start () {
+        currentHealth = startingHealth;
+        healthText.text = "Health: " + currentHealth;
+	}
+
+    void OnTriggerEnter(Collider other){
+        if (other.tag == "Smoke"){
+        harmed = true;
+            InvokeRepeating("Damage", 1.0f, 1.0f);
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.tag == "Smoke"){
+        harmed = false;
+        }
+    }
+
+    public void Damage(){
+        if (harmed) {
+            currentHealth = currentHealth - 1;
+            healthText.text = "Health: " + currentHealth;
+            if (currentHealth == 0 ){
+                Application.LoadLevel(Application.loadedLevel);
+            }
+        }
+    }
+}
