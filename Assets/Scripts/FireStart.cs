fe5fb13bab0ec94002d2004c6ace823aa349b8c1
OddurV <a16oddvi@hs.local>
2016-11-29 16:14:35 +0100

    Added Fire and smoke prefabs

--- Assets/Scripts/FireStart.cs
+++ Assets/Scripts/FireStart.cs
@@ -5,25 +5,66 @@ public class FireStart : MonoBehaviour {
 
 	public float timeRemaining = 40f;
 	public GameObject fire;
+	public GameObject smoke;
+	public bool isReachingRightWall;
+	public bool isReachingLeftWall;
+
 	public Transform [] spawnFirePoints;
 	public GameObject gameManager;
+
+
+
+
+	public static int spreadDistance = 0;
 
 	// Use this for initialization
 	void Start () {
 		
 		Invoke ("StartFire", timeRemaining);
-
 	}
 	
 	// Update is called once per frame
 	void Update () {
+		
 	}
 		
 	//The fire starts in a random predefined spawn point
 	public void StartFire () {
 		gameManager.GetComponent<GameManager> ().isThereAFire = true;
 		int spawnPointIndex = Random.Range (0, spawnFirePoints.Length);
+		//int spawnPointIndex = 12;
+
 		Instantiate (fire, spawnFirePoints[spawnPointIndex].position, spawnFirePoints[spawnPointIndex].rotation);
+		Instantiate (smoke, spawnFirePoints[spawnPointIndex].position, smoke.transform.rotation);
+
+		InvokeRepeating ("SpreadFire", 10f, 1f);
+
+	}
+
+
+	void SpreadFire() {
+		
+		GameObject respawn;
+		Vector3 newPositionRight;
+		Vector3 newPositionLeft;
+	
+
+		spreadDistance += 2;
+		//Looking for the fire spot
+		respawn = GameObject.FindGameObjectWithTag("Fire");
+
+		//Adding smoke in each side of the first fire spot
+
+		newPositionRight = respawn.transform.position + new Vector3 (0f, 0f, spreadDistance);
+		newPositionLeft = respawn.transform.position - new Vector3 (0f, 0f, spreadDistance);
+
+		//Delimits the smoke spreading for the right wall
+		if (!isReachingRightWall)
+			Instantiate (smoke, newPositionRight, smoke.transform.rotation);
+
+		//Delimits the smoke spreading for the left wall
+		if (!isReachingLeftWall)
+			Instantiate (smoke, newPositionLeft, smoke.transform.rotation);
 	}
 
 }
---
+++ Assets/Scripts/LeftWallCollider.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LeftWallCollider : MonoBehaviour {
+
+	public GameObject fireManager;
+
+	void OnTriggerEnter(Collider other) {
+
+
+		if(other.gameObject.tag == "Smoke")
+			fireManager.GetComponent<FireStart>().isReachingLeftWall = true;
+	}
+}
