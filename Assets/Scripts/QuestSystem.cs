1fb1a8365c20712d4f37dbec7d2b4ccc2dc39433
OddurV <a16oddvi@hs.local>
2016-11-29 10:43:32 +0100

    Started work on QuestUI
    Added quest icons and started to add references to them to the quest
    manager.

--- Assets/Scripts/QuestSystem.cs
+++ Assets/Scripts/QuestSystem.cs
@@ -9,6 +9,21 @@ public class QuestSystem : MonoBehaviour {
 	public GameObject[] quests;
 	public int questCounter = 0;
 	public GameObject fireManager;
+	public GameObject mainCamera;
+	private Transform target; // Target to point at
+	private Vector3 targetPos;
+	private Vector3 screenMiddle;
+
+	//Quest Icons
+	public GameObject arrow;
+	public GameObject exitSign;
+	public GameObject questMarker;
+	public GameObject alert;
+	public GameObject coffee;
+	public GameObject letter;
+	public GameObject speechCoffee;
+	public GameObject speechRun;
+	public GameObject speechLetter;
 
 	void Start(){
 		quests = GameObject.FindGameObjectsWithTag ("Quest");
@@ -20,6 +35,11 @@ public class QuestSystem : MonoBehaviour {
 		// Activate the first quest
 		quests [0].SetActive (true);
 		messageText.text = "Go to "+quests[0];
+	}
+
+	void Update(){
+		// Get the targets position on screen into a Vector3
+		//targetPos = mainCamera.WorldToScreenPoint(target.transform.position);
 	}
 
 	//Use:  QuestCompleted()
