e0ca67f711d717f97bda80463a317af54d9f3021
OddurV <a16oddvi@hs.local>
2016-12-10 17:29:39 +0100

    Fixing the elevator bugs

--- Assets/Scripts/HoldCharacter.cs
+++ Assets/Scripts/HoldCharacter.cs
@@ -3,11 +3,20 @@ using System.Collections;
 
 public class HoldCharacter : MonoBehaviour {
 
-	void OnTriggerEnter(Collider col){
-		col.transform.parent = gameObject.transform;
+	//public float offset;
+
+	void OnTriggerEnter(Collider other){
+		//other.transform.parent = gameObject.transform;
+		//offset = other.transform.position.y - gameObject.transform.position.y; // When this code is run, the offset is -1.45 on the first floor. 
 	}
 
-	void OnTriggerExit(Collider col){
-		col.transform.parent = null;
+	void OnTriggerExit(Collider other){
+		//other.transform.parent = null;
+	}
+
+	void OnTriggerStay(Collider other){
+		Vector3 temp = other.transform.position;
+		temp.y = gameObject.transform.position.y - 1.45f;//+offset;
+		other.transform.position = temp;
 	}
 }
